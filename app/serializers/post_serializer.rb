f44e9a5aa4736c6dfb24c6eb451185e320e18a38
majaliju <majalijunyc@gmail.com>
2023-01-18 18:46:51 -0500

    added the 5 methods on post; for easier frontend access

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :body, :for_sale, :tickets, :concert_id, :user_id, :poster_name, :poster_email, :concert_image,
-             :concert_date
+             :concert_location
+  :concert_date
 
   belongs_to :user, serializer: UserSerializer
   belongs_to :concert, serializer: ConcertSerializer
