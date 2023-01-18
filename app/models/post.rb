23db66fae1261ded9c89d4232c436c21eea934a5
majaliju <majalijunyc@gmail.com>
2023-01-18 18:45:47 -0500

    added the 5 methods on post; for easier frontend access

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :concert
 
-  validates :body, length: { minimum: 6, too_short: "has to be %{count} letters! Leave a phone number or price"}
+  validates :body, length: { minimum: 6, too_short: 'has to be %<count>s letters! Leave a phone number or price' }
   validates :tickets, numericality: { greater_than: 0, only_integer: true }
 
   def poster_name
@@ -13,4 +13,15 @@ class Post < ApplicationRecord
     user.email
   end
 
+  def concert_image
+    concert.image
+  end
+
+  def concert_location
+    concert.location
+  end
+
+  def concert_date
+    concert.date
+  end
 end
