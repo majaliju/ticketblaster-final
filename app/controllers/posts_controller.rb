06ba62bbe4ea7d669dc81e1f7f626c7aa3a8aabf
majaliju <majalijunyc@gmail.com>
2023-02-18 11:57:16 -0500

    streamlining the PostsController to include a before_action and DRY up authroization of user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-  
+  before_action :authorize_user, only: %i[update destroy]
+
   def index
     posts = Post.all
     render json: posts
@@ -12,55 +13,38 @@ class PostsController < ApplicationController
     render json: post, status: 200
   end
 
-
   def create
     post = Post.create!(new_post_params)
     render json: post, status: 201
   end
 
-  # ## made this one to not-render duplicates but still rendered duplicates
-  # def create
-  #   ## links the proper user to the post
-  #   correct_user = User.find_by!(id: params[:user_id])
-
-  #   ## links the proper concert to the post
-  #   correct_concert = Concert.find_by!(id: params[:concert_id])
-
-  #   newPost = Post.create!(
-  #     id: params[:id],
-  #     body: params[:body],
-  #     tickets: params[:tickets],
-  #     for_sale: params[:for_sale],
-  #     concert_id: correct_concert.id,
-  #     user_id: correct_user.id
-  #   )
-  #   render json: newPost, status: 201
-  # end
-
   def update
     post = Post.find_by!(id: params[:id])
-    if session[:user_id] === post[:user_id]
-      ## potential cross-check for if only body, or only tickets if being updated
-      ## but also seems irrelevant
-      post.update!(
-        body: params[:body],
-        tickets: params[:tickets]
-      )
-      render json: post, status: 200
-    end 
+
+    ## potential cross-check for if only body, or only tickets if being updated
+    ## but also seems irrelevant
+    post.update!(
+      body: params[:body],
+      tickets: params[:tickets]
+    )
+    render json: post, status: 200
   end
-        
 
   def destroy
     post = Post.find_by!(id: params[:id])
-    if session[:user_id] === post[:user_id]
-     
-      post.destroy
-      head :no_content
-    end
+
+    post.destroy
+    head :no_content
   end
 
   private
+
+  def authorize_user
+    unless session[:user_id] === params[:user_id]
+      render json: { error: 'User not authorized to make edits!' },
+             status: :unauthorized
+    end
+  end
 
   def new_post_params
     params.permit(:concert_id, :user_id, :for_sale, :tickets, :body)
@@ -73,30 +57,27 @@ class PostsController < ApplicationController
   def render_not_found_response(invalid)
     render json: { error: invalid.message }, status: :not_found
   end
-
 end
 
+# # original write-up for create
+# def create
+#   post = Post.create!(new_post_params)
 
-  # # original write-up for create
-  # def create
-  #   post = Post.create!(new_post_params)
+#   if post.valid?
+#     render json: post, status: 200
+#   else
+#     render json: { errors: post.errors }, status: :unprocessable_entity
+#   end
+# end
 
-  #   if post.valid? 
-  #     render json: post, status: 200
-  #   else
-  #     render json: { errors: post.errors }, status: :unprocessable_entity
-  #   end
-  # end
-
-
-  # def update
-  #   post = Post.find_by(id: params[:id])
-  #   user = User.find_by(id: session[:user_id])
-  #   if session[:user_id] === params[:user_id]
-  #     post.update(
-  #       comment_body: params[:comment_body],
-  #       tickets: params[:tickets]
-  #     )
-  #     render json: post, status: 200
-  #     end 
-  #   end
+# def update
+#   post = Post.find_by(id: params[:id])
+#   user = User.find_by(id: session[:user_id])
+#   if session[:user_id] === params[:user_id]
+#     post.update(
+#       comment_body: params[:comment_body],
+#       tickets: params[:tickets]
+#     )
+#     render json: post, status: 200
+#     end
+#   end
