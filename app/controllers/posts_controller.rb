59b04e0910766f0c1b2f885474137329137395a4
majaliju <majalijunyc@gmail.com>
2023-03-07 20:47:14 -0500

    --

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,5 @@
 class PostsController < ApplicationController
   before_action :authorize_user, only: [:update]
- 
-  def word_match
-#     CHALLENGE WITH BEN
-
-# Create a custom route that takes a parameter of a single word. That route should take us to an action where we look through posts to see if any of the posts have that word in the content (doesn’t have to be a full match, case-insensitive). The action will render json of all the users that have matching posts. If there is no match render json that says so.
-
-    # render
-    # byebug
-    # take a post, see if post.body matches params
-    # Post.all.first.body.include?("tickets")
-    word = params
-    matching_users_array = []
-  
-posts = Post.all
-matches = posts.filter {|post| post.body.include?(params[:word])}
-matching_users = matches.each {|post| matching_users_array << post.user}
-byebug
-if matching_users_array
-  render json: matching_users_array, status: 200
-else
-  render json: "I'm sorry, no user has a comment with this word!"
-# if matching_users_array
-#   render json: matching_users, status: 201
-# else 
-#   render json: "I'm sorry, no user has this phrase in their comment!"
-end
-
-  end
 
   def index
     posts = Post.all
@@ -92,17 +64,3 @@ end
 end
 
 
-## have to reconfigure frontend error handling, to render this error message
-# def update
-#   post = find_post
-
-# if post[:body] === params[:body] && post[:tickets] === params[:tickets]
-#   render json: [{error: "Nothing was edited!"}], status: :unprocessable_entity
-# else
-#   post.update!(
-#     body: params[:body],
-#     tickets: params[:tickets]
-#   )
-#   render json: post, status: 200
-# end
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -10,53 +10,9 @@ Rails.application.routes.draw do
 
   get '/session_cookies', to: 'sessions#show_cookies'
 
-  get '/word_match/:word', to: 'posts#word_match'
-
-
-
-
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
 
 
-
-
-## MY ORIGINAL NON-RESTful APPROACH, WITH CUSTOM ROUTES
-# ##~ FOR ARTIST and CONCERTS GET/CREATIONS
-# #& to get the artists and concerts, respectively
-# get '/artists', to: "artists#index"
-# get '/concerts', to: "concerts#index"
-#  #& to create a new artist and new concert, respectively
-# post '/new_artist', to: "artists#create"
-# post '/new_concert', to: "concerts#create"
-
-# # #& get info about the artist's upcoming shows
-# # get '/upcoming_shows', to: "artists#upcoming_shows"
-# # # set it to where if the dates are after today's date then pull these concerts
-
-# ##~ FOR THE POSTS GET/CREATION/EDITS/DELETION
-# get '/posts', to: "posts#index"
-# post '/new_post', to: "posts#create"
-# patch '/update_post/:id', to: "posts#update"
-# delete '/delete_post/:id', to: "posts#destroy"
-
-# ##~ THE LOGIN/LOGOUT ROUTES
-# #& to create a new user outright
-# post "/new_user", to: "users#create"
-# #& to login our user
-# post "/login", to: "sessions#create"
-# #& to keep the user logged in
-# get "/me", to: "users#show"
-# #& to log the user out
-# delete "/logout", to: "sessions#destroy"
-
-# #& show all the users
-# get "/users", to: "users#index"
-
-# # #~ SESSION & COOKIES INFO
-# # & shows session_id and sessions info
-# get '/show_session', to: 'application#show_session'
-# # & displays cookies
-# get '/cookies', to: 'application#show_cookies'
