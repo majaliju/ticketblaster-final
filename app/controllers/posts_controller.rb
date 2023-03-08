4e6377dda0eaa23eb8a77d7caee7d803c69c9e21
majaliju <majalijunyc@gmail.com>
2023-03-07 19:20:20 -0500

    with the challenge from ben

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,6 @@ class PostsController < ApplicationController
 
 # Create a custom route that takes a parameter of a single word. That route should take us to an action where we look through posts to see if any of the posts have that word in the content (doesn’t have to be a full match, case-insensitive). The action will render json of all the users that have matching posts. If there is no match render json that says so.
 
-    # render
-    # byebug
-    # take a post, see if post.body matches params
-    # Post.all.first.body.include?("tickets")
-    word = params
-    matching_users_array = []
-  
 posts = Post.all
 matches = posts.filter {|post| post.body.include?(params[:word])}
 users = matches.filter {|post| post.user}
---
+++ challenge.md
@@ -0,0 +1,3 @@
+CHALLENGE WITH BEN
+
+Create a custom route that takes a parameter of a single word. That route should take us to an action where we look through posts to see if any of the posts have that word in the content (doesn’t have to be a full match, case-insensitive). The action will render json of all the users that have matching posts. If there is no match render json that says so.
